768a1ef6bd03d9b4129121923845cd0bf86396c1
Ashley Davis <ashley.davis@realseriousgames.com>
2015-08-04 10:18:57 +1000

    Logging in the server restart script is now relative to the working directory so that the script may change directories.

--- Deploy/server_restart.sh
+++ Deploy/server_restart.sh
@@ -2,7 +2,7 @@
 # Note: if you update this file and deploy, this script will be invoke on the serer.
 
 D=$(date +"%m_%d_%Y")
-LOG=./logs/restart_$D.log
+LOG=$(pwd)/logs/restart_$D.log
 echo === $D === > $LOG
 pm2 delete all >> $LOG
 pm2 start ./Server/server.js >> $LOG
