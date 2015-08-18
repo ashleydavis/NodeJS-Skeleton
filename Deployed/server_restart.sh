04299e56c6abccafdd94f829e125599c29355a63
Ashley Davis <ashley.davis@realseriousgames.com>
2015-08-18 11:18:04 +1000

    Fixed server restart script so that it captures stderr as well as stdout to the log.

--- Deployed/server_restart.sh
+++ Deployed/server_restart.sh
@@ -3,10 +3,10 @@
 
 D=$(date +"%m_%d_%Y")
 LOG=$(pwd)/logs/restart_$D.log
-echo === $D === > $LOG
-pm2 delete all >> $LOG
-cd ./Server >> $LOG
-npm install -y --no-bin-links >> $LOG
-pm2 start ./server.js >> $LOG
-pm2 save >> $LOG
-echo Restart after commit $HG_NODE >> $LOG
+echo === $D === > $LOG 2>&1 
+pm2 delete all >> $LOG 2>&1 
+cd ./Server >> $LOG 2>&1 
+npm install -y --no-bin-links >> $LOG 2>&1 
+pm2 start ./server.js >> $LOG 2>&1 
+pm2 save >> $LOG 2>&1 
+echo Restart after commit $HG_NODE >> $LOG 2>&1 
