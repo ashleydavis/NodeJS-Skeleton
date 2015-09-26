2f3624625cfcd97bcec81a7ff04cf816f3ebe95b
Ashley Davis <ashley@codecapers.com.au>
2015-09-26 16:39:34 +1000

    Fixed user name.

--- connect.bat
+++ connect.bat
@@ -2,4 +2,4 @@
 setlocal
 if "%1" == "" set sshhost=nodejs-skeleton
 if not "%1" == "" set sshhost=%1
-ssh ivBY2qT@%sshhost%.cloudapp.net -i .\keys\your-private.key -o "StrictHostKeyChecking no" -C 
+ssh someuser@%sshhost%.cloudapp.net -i .\keys\your-private.key -o "StrictHostKeyChecking no" -C 
