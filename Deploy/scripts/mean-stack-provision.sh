2cd10ec99640f736879b163ecfc33e143f929b26
Ashley Davis <ashley@codecapers.com.au>
2015-09-26 15:39:32 +1000

    Fixed directory reference to the .pm2 directory. Added a command to fix ownership of the .npm directory.

--- Deploy/scripts/mean-stack-provision.sh
+++ Deploy/scripts/mean-stack-provision.sh
@@ -17,7 +17,9 @@ sudo npm install -g -y bower
 echo "============== PM2 setup =================="
 sudo npm install pm2 -g --unsafe-perm
 sudo env PATH=$PATH:/usr/bin pm2 startup ubuntu -u $USER
-sudo chown -R $USER ./.pm2
+sudo chown -R $USER ~/.pm2
 echo "============== Fixing mongodb conf =================="
 sudo sed -i 's/bind_ip = 127.0.0.1/#bind_ip  = 127.0.0.1/g' /etc/mongodb.conf
 sudo service mongodb restart
+echo "============== Fixing ownership of .npm directory =================="
+sudo chown -R $USER ~/.npm
